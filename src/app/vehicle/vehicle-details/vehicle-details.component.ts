b820f8449b790f89593ab7487e0b17267ace75ab
Rricardo2431 <128594768+Rricardo2431@users.noreply.github.com>
2023-09-06 11:39:22 +0200

    resolve2

--- src/app/vehicle/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle/vehicle-details/vehicle-details.component.ts
@@ -12,6 +12,17 @@ export class VehicleDetailsComponent implements OnInit {
   vehicleDetails: any; 
 
   constructor(private http: HttpClient, private router: Router) {}
-
+  ngOnInit(): void {
+    this.vehicleId = this.router.url.split('/').pop();
+    console.log('Url: ' + this.router.url);
+    console.log('Vehicle Id: ' + this.vehicleId);
+    this.http
+      .get('http://localhost:8080/vehicles/' + this.vehicleId)
+      .subscribe((data) => {
+        console.log(data);
+        this.vehicleDetails = data;
+        this.vehicleDetails.brand = this.vehicleDetails.brand.toUpperCase();
+      });
+  }
 }
 
