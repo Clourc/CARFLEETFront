44d37bd09ccd9b12cdc0d257209ca4159e8b9502
darenik67 <karma-67.75@hotmail.fr>
2023-09-06 10:23:00 +0200

    remove vehicle details

--- src/app/vehicle/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle/vehicle-details/vehicle-details.component.ts
@@ -1,32 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { VehicleService } from '../vehicle.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { Component, } from '@angular/core';
+
 @Component({
   selector: 'app-vehicle-details',
   templateUrl: './vehicle-details.component.html',
   styleUrls: ['./vehicle-details.component.css']
 })
-export class VehicleDetailsComponent implements OnInit {
+export class VehicleDetailsComponent {
 
 
-vehicleId: number;
-vehicle: any;
 
+   
 
-  constructor(private vehicleService: VehicleService ,private route: ActivatedRoute ) { 
-
-    this.vehicleId = 0;
   }
-
-  ngOnInit(): void {
-    this.vehicleId = this.route.snapshot.params['id'];
-
-  // Utilisez l'ID du véhicule pour obtenir les détails du véhicule depuis le service
-  this.vehicleService.getVehiclebyId(this.vehicleId).subscribe(data => {
-    this.vehicle = data;
-    console.log(data);
-  });
-}
-}
