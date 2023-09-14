73cf8a077a2dbc3678e9e255902b0004246f776e
darenik67 <karma-67.75@hotmail.fr>
2023-09-14 12:18:16 +0200

    resolve filter problem

--- src/app/vehicle/vehicle.service.ts
+++ src/app/vehicle/vehicle.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { filter, switchMap } from 'rxjs/operators';
 import { ReservationService } from '../reservation/reservation.service';
 
 @Injectable({
@@ -42,13 +42,23 @@ export class VehicleService {
       reservationList = data;
 
       const filterResult: any[] = [];
-      console.log('test vide');
-      console.log(vehiclesToDisplay);
-
+console.log("resrvationList", reservationList);
       for (let i = 0; i < reservationList.length; i++) {
+        // if (reservationList[i].vehicle.model.modelName === "CLIO RS Line"
+        // || reservationList[i].vehicle.model.modelName === 
+        // "ZOE R110") {
+        //   console.log("vehicle reservation", reservationList[i])
+        //   let checkResaStartDatesTest: boolean =
+        //   reservationStart >= reservationList[i].start_Date &&
+        //   reservationStart <= reservationList[i].end_Date;
+
+        //   console.log("checktest startDate", checkResaStartDatesTest)
+        // }
         let checkResaStartDates: boolean =
           reservationStart >= reservationList[i].start_Date &&
           reservationStart <= reservationList[i].end_Date;
+
+
         let checkResaEndDates: boolean =
           reservationEnd >= reservationList[i].start_Date &&
           reservationEnd <= reservationList[i].end_Date;
@@ -56,126 +66,47 @@ export class VehicleService {
           reservationStart <= reservationList[i].start_Date &&
           reservationEnd >= reservationList[i].end_Date;
         if (checkResaStartDates || checkResaEndDates || checkResaWide) {
-          console.log('vehicle: ');
           filterResult.push(reservationList[i].vehicle);
-          console.log('filter results', filterResult);
         }
       }
-      savedVehicles.forEach((vehicle: any) => {
-        // Liste de tous les véhicule
-        if (filterResult.length > 0) {
-          // filterResult est la liste des véhicule réservé dans les dates choisies
-          for (let hideVehicle of filterResult) {
-            if (hideVehicle.id != vehicle.id) {
-              // On check si le véhicule de la liste de tous les véhicule
-              // n'est pas dans la liste des véhicule réservé pour l'afficher
-              console.log('Test');
-              vehiclesToDisplay.push(vehicle);
-            }
-          }
-        } else {
-          // Si la liste de véhicule réservé est vide on affiche tous les véhicules
-          // Parce qu'ils sont tous disponible
-          vehiclesToDisplay = savedVehicles;
-        }
-      });
-      console.log('display');
-      console.log(vehiclesToDisplay);
+
+      if (filterResult.length == 0) {
+        vehiclesToDisplay = savedVehicles;
+      } else {
+        // Ici je crée un tableau de vehicleId parce que c'est le tableau de tous les véhicules
+        // Je vais conserver l'id pour le filtre et l'index pour pouvoir récupérer le véhicule entier plus tard
+        const savedVehiclesIds = savedVehicles.map((vehicle, idx) => {
+         return  {vehicleId: vehicle.id, idx}
+        });
+
+        // Ici j'ai le tableau d'id des véhicule que je ne veux pas parce q'uils sont déjà réserver 
+        const filterResultIds = filterResult.map(vehicle => vehicle.id);
+
+        savedVehiclesIds.forEach((vehicleWithIdAndIndex: any) => {
+         
+          if (!filterResultIds.includes(vehicleWithIdAndIndex.vehicleId)) vehiclesToDisplay.push(savedVehicles[vehicleWithIdAndIndex.idx])
+        })
+        // console.log('Filter result :', filterResult);
+        // for (let vehicle of savedVehicles) {
+        //   // Liste de tous les véhicule
+        //   if (filterResult.length > 0) {
+        //     // filterResult est la liste des véhicule réservé dans les dates choisies
+        //     for (let hideVehicle of filterResult) {
+        //       const testVehicleFilter = (element: any) =>
+        //         element.id == vehicle.id;
+        //       if (
+        //         hideVehicle.id != vehicle.id &&
+        //         !vehiclesToDisplay.some(testVehicleFilter)
+        //       ) {
+        //         // On check si le véhicule de la liste de tous les véhicule
+        //         // n'est pas dans la liste des véhicule réservé pour l'afficher
+        //         vehiclesToDisplay.push(vehicle);
+        //       }
+        //     }
+        //   }
+        // }
+      }
+      // return la liste de véhicule disponible par date
     });
-    // return la liste de véhicule disponible par date
   }
-
-  /* reservationList: any;
-savedVehicles: any[]=[];
-vehiclesToDisplay: any[]= [];
-  fetchReservationsAndVehicles(): void {
-    this.reservationService.getListResa().pipe(
-      switchMap((reservationList) => {
-        console.log("data", reservationList);
-        this.reservationList = reservationList; // Store reservationList
-
-        return this.getVehicles().subscribe( (vehiclesData) => {
-        this.savedVehicles = vehiclesData;
-        this.vehiclesToDisplay = vehiclesData;
-        console.log("liste", vehiclesData);
-
-      },
-      (error) => {
-        console.error('Erreur dans la récupération de véhicules', error);
-      }
-
-      })
-    ).subscribe(
-     
-        
-    );
-  }
- */
 }
-
-// recherche(reservationStart: any, reservationEnd: any) {
-//   let reservationList: any = [];
-//   this.reservationService.getListResa().subscribe((data) => {
-//     console.log("dataaaaaa", data)
-//     reservationList = data;
-//     this.getVehicles().subscribe(
-//       (data) => {
-//         this.savedVehicles = data;
-//         this.vehiclesToDisplay = data;
-//         console.log("liste")
-//         console.log(data);
-//       },
-//       (error) => {
-//         console.error('Erreur dans la récupération des véhicules:', error);
-//       }
-//     );
-//     for (let i = 0; i < reservationList.length; i++) {
-//       let checkResaStartDates: boolean =
-//         reservationStart >= reservationList[i].start_Date &&
-//         reservationStart <= reservationList[i].end_Date;
-//       let checkResaEndDates: boolean =
-//         reservationEnd >= reservationList[i].start_Date &&
-//         reservationEnd <= reservationList[i].end_Date;
-//       let checkResaWide: boolean =
-//         reservationStart <= reservationList[i].start_Date &&
-//         reservationEnd >= reservationList[i].end_Date;
-//       if (checkResaStartDates || checkResaEndDates || checkResaWide) {
-//         console.log('vehicle: ');
-//         filterResult.push(reservationList[i].vehicle);
-//         console.log(filterResult);
-//       }
-//     }
-//     savedVehicles.forEach((vehicle: any) => {
-//       if (filterResult.length > 0) {
-//         for (let hideVehicle of filterResult) {
-//           if (hideVehicle.id != vehicle.id) {
-//             console.log('Test');
-//             vehiclesToDisplay.push(vehicle);
-//           }
-//         }
-//       } else {
-//         vehiclesToDisplay = savedVehicles;
-//       }
-//     });
-//   });
-
-//   const vehiclesToDisplay: any[] = [];
-//   const filterResult: any[] = [];
-//   console.log('test vide');
-//   console.log(vehiclesToDisplay);
-
-//   console.log('display');
-//   console.log(vehiclesToDisplay);
-//   // return la liste de véhicule disponible par date
-// }
-
-/*getVehicles() {
-    fetch('http://localhost:8080/vehicules')
-      .then(response => response.json())
-      .then(data => {
-        this.listVehicles = data;
-        console.log(data);
-      }
-      )
-      return this.listVehicles;
-  }*/
